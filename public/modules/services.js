59d0c0c3c86ab77e8b99bee71d71ac781beb2f9c
Ubuntu <jan.blonde@icloud.com>
2016-01-15 14:55:51 +0000

    refactoring

---
+++ public/modules/services.js
@@ -0,0 +1,40 @@
+angular.module('services',[])
+  .factory('serviceApi', serviceApi)
+  .constant('apiUrl','http://54.93.65.196:3000/api/')
+
+function serviceApi($http,apiUrl) {
+
+  function request(verb, object, param, data){
+    var req = {
+      method: verb,
+      url: url(object, param),
+      headers: {
+        'Authorization': ''
+      },
+      data: data
+    }
+    return $http(req);
+  } 
+
+  function url(object, param) {
+    if (param==null || !angular.isDefined(param)){
+      param = '';
+    }
+    return apiUrl + object + "/" + param;
+  }
+
+  return {
+    get: function (object, param) {
+      return request("GET",object, param);
+    },
+    post: function (object, data) {
+      return request("POST", object, null, data);
+    },
+    delete: function (object, param) {
+      return request("DELETE", object, param);
+    },
+    put: function (object, data) {
+      return request("PUT", object, null, data);
+    }
+  }
+}
